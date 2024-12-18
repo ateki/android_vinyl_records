92028ab934ea23ba0fc0401b0a96624035353395
soufia atek <irene.atek@btinternet.com>
2024-12-18 10:39:23 +0000

    feat: added POST method to AlbumApiService

--- app/src/main/java/com/northcoders/vinylrecords/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/vinylrecords/service/AlbumApiService.java
@@ -4,12 +4,18 @@ import com.northcoders.vinylrecords.model.Album;
 
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
 
     @GET("albums")
     Call<List<Album>> getAllAlbums();
+
+
+    @POST("albums")
+    Call<List<Album>> addAlbum(@Body Album album);
 
     //    Call is a Retrofit class which allows the application to make asynchronous HTTP requests and get the response from the server.
     //    This is typically executed on a background thread, which managed by Retrofit, and the response is delivered to the main thread of the application.
