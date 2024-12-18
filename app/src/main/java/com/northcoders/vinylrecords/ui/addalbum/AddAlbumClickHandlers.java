b6c7e29490aad3cfc704a4ba39ab156b2a18b24c
soufia atek <irene.atek@btinternet.com>
2024-12-18 17:52:57 +0000

    feat: add album functionality - add ClickHandlers for both GoBack and Add button

---
+++ app/src/main/java/com/northcoders/vinylrecords/ui/addalbum/AddAlbumClickHandlers.java
@@ -0,0 +1,52 @@
+package com.northcoders.vinylrecords.ui.addalbum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.northcoders.vinylrecords.model.Album;
+import com.northcoders.vinylrecords.ui.mainactivity.MainActivity;
+import com.northcoders.vinylrecords.ui.mainactivity.MainActivityViewModel;
+
+public class AddAlbumClickHandlers {
+
+    private Album album;
+    private Context context;
+    private MainActivityViewModel viewModel;
+
+    public AddAlbumClickHandlers(Album ablum, Context context, MainActivityViewModel viewModel) {
+        this.album = ablum;
+        this.context = context;
+        this.viewModel = viewModel;
+    }
+
+    public void onAddBtnClickec(View view) {
+        // condition check whether fields are empty
+        if (album.getTitle() == null || album.getArtist()==null || album.getDescription()==null ||
+        album.getGenre()==null || album.getReleaseDate()==null || album.getCoverImg()==null) {
+
+            Toast.makeText(context, "Fields cannot be empty!", Toast.LENGTH_SHORT).show();
+
+            // TODO: Any other validation
+            // TODO: Perhaps allow some fields to be empty
+            // Confirm stockQuantity >= 0
+        } else if (album.getStockQuantity()<0) {
+
+            Toast.makeText(context, "Stock Quantity must not negative number.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Intent intent = new Intent(context, MainActivity.class);
+
+            viewModel.addAlbum(album);
+
+            context.startActivity(intent);
+        }
+    }
+
+
+    public void onGoBackBtnClicked(View view) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+}
