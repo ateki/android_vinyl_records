51a7fd05402919f86c7c1ce707881fcddd5501cb
soufia atek <irene.atek@btinternet.com>
2024-12-20 13:21:26 +0000

    feat: searchable ablum list functionality

--- app/src/main/java/com/northcoders/vinylrecords/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/vinylrecords/ui/mainactivity/MainActivity.java
@@ -2,8 +2,12 @@ package com.northcoders.vinylrecords.ui.mainactivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+//import android.widget.SearchView;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -21,13 +25,20 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements RecyclerViewInterface {
 
     private static final String TAG = "MainActivity";
-    private RecyclerView recyclerView;
     private ArrayList<Album> albumList;  // List??
+
+    private ArrayList<Album> filteredAlbumList;
     private AlbumAdapter albumAdapter;
     private ActivityMainBinding binding;
     private MainActivityViewModel viewModel;
 
     private MainActivityClickHandler handler;
+    
+    // UI Widgets
+
+    private RecyclerView recyclerView;
+    private SearchView searchView;
+    
 
     private final static String ALBUM_KEY = "album";
 
@@ -49,7 +60,52 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
 
         binding.setClickHandler(handler);
 
+        // Setup album list recycler view and search view
+        // TODO: Rename  setupAlbumRecyclerView
         getAllAlbums();
+
+        setupSearchView();
+
+    }
+
+
+    private void setupSearchView() {
+        // Clear search view
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterAlbumList(newText);
+                return true;
+            }
+        });
+    }
+
+    private void filterAlbumList(String newText) {
+        Log.i(TAG, "filterAlbumList searchText = " +newText);
+        filteredAlbumList = new ArrayList<>();
+
+        // Search for albums with newText somewhere in the title
+        for (Album album : albumList) {
+            if (album.getTitle().toLowerCase().contains(newText.toLowerCase())) {
+                Log.i(TAG, "filteredAlbumList : adding album " + album.getTitle());
+                filteredAlbumList.add(album);
+            }
+        }
+
+        if (filteredAlbumList.isEmpty()) {
+            Toast.makeText(this, "No albums found!", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.i(TAG, "filterAlbumList::filteredAlbumList filteredAlbumList= " +filteredAlbumList);
+            albumAdapter.setFilteredAlbumList(filteredAlbumList);
+        }
     }
 
     private void getAllAlbums() {
@@ -72,12 +128,20 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         albumAdapter.notifyDataSetChanged();
+        // TODO Use more specific change events rather than notifyDataSetChanged,
+        //  so that LayoutManager does not need to rebind everything and layout all visible views
     }
 
     public void onItemClick(int position) {
         Intent intent = new Intent(MainActivity.this, UpdateAlbumActivity.class);
 
-        intent.putExtra(ALBUM_KEY, albumList.get(position));
+        if (filteredAlbumList == null || filteredAlbumList.isEmpty()) {
+            // Display all albums
+            intent.putExtra(ALBUM_KEY, albumList.get(position));
+        } else {
+            // Display filtered albums only
+            intent.putExtra(ALBUM_KEY, filteredAlbumList.get(position));
+        }
 
         startActivity(intent);
     }
